3712e009c928c469e4ba1c7c326a95d22948f59b
sunzhikai <sunzhikai2001@163.com>
2022-11-21 10:50:33 +0800

    first commit

---
+++ src/main/java/com/xiaozhizhu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiaozhizhu.git;
+
+/**
+ * @author Sunzhikai
+ * @version 1.0
+ * @date 2022/11/21 10:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
