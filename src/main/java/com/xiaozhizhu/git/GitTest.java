5b26673dbb8f5828f3b738a33f8e931c73fdac68
sunzhikai <sunzhikai2001@163.com>
2022-11-21 11:22:39 +0800

    master commit2

--- src/main/java/com/xiaozhizhu/git/GitTest.java
+++ src/main/java/com/xiaozhizhu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello22");
         System.out.println("hello22");
         System.out.println("hello22");
+        System.out.println("1323232");
     }
 }
