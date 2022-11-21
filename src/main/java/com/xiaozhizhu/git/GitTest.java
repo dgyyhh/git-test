1e5b222a2b77a23e4bb916acd9e937d8186d5db6
sunzhikai <sunzhikai2001@163.com>
2022-11-21 11:00:04 +0800

    second commit

--- src/main/java/com/xiaozhizhu/git/GitTest.java
+++ src/main/java/com/xiaozhizhu/git/GitTest.java
@@ -8,5 +8,9 @@ package com.xiaozhizhu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello22");
+        System.out.println("hello22");
+        System.out.println("hello22");
     }
 }
