08fb8b38b5e2e6423eb0861de7be49d32f077c92
sunzhikai <sunzhikai2001@163.com>
2022-11-21 11:21:27 +0800

    hot-fix commit2

--- src/main/java/com/xiaozhizhu/git/GitTest.java
+++ src/main/java/com/xiaozhizhu/git/GitTest.java
@@ -14,5 +14,7 @@ public class GitTest {
         System.out.println("hello22");
         System.out.println("hello22");
         System.out.println("hello22");
+        System.out.println("hello22");
+        System.out.println("333");
     }
 }
